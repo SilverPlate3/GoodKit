e12cc2e7a3cabe630c41b481c7c2923e484f2539
SilverPlate3 <arielsilver77@gmail.com>
2024-03-18 08:39:11 +0200

    Move from main to other places

--- main.c
+++ main.c
@@ -1,15 +1,10 @@
-#include <linux/delay.h> 
-#include <linux/kernel.h> 
 #include <linux/module.h> 
-#include <linux/cred.h> /* For current_uid() */ 
-#include <linux/uidgid.h> /* For __kuid_val() */ 
 #include <linux/fs.h>
 #include <linux/limits.h>
 #include <linux/sched.h> 
 #include <linux/kprobes.h> 
 
-#include "StringUtils.h"
-#include "Rules.h"
+#include "ExecveEvent.h"
 
 #ifndef CONFIG_KPROBES
 #error "CONFIG_KPROBES is not defined but is required."
@@ -25,55 +20,25 @@
 static unsigned long **sys_call_table_stolen; 
 static asmlinkage long (*original_execve)(const struct pt_regs *);
 
-
 static asmlinkage long our_sys_execve(const struct pt_regs *regs) 
 {
-    const char __user *__filename = (const char __user *)regs->di;
-    const char __user *const __user *__argv = (const char __user *const __user *)regs->si;
-
-    uid_t uid = __kuid_val(current_uid());
-    gid_t gid = __kgid_val(current_gid());
-
-    int filename_len = strnlen_user(__filename, PATH_MAX);
-    char *filename = kmalloc(filename_len+1, GFP_KERNEL);
-    if (unlikely(!filename))
+    execve_event * event = create_execve_event(regs);
+    if(!event)
     {
-        pr_info("Failed to allocate memory for filename\n");
         goto call_original_execve;
     }
-    if(copy_from_user(filename, __filename, filename_len))
-    {
-        pr_info("Failed to copy filename from user space\n");
-        goto free_file_name_and_call_original_execve;
-    }
-    filename[filename_len] = '\0'; 
 
-    MKVAR(const char __user *const __user *, argv, __argv);
-    char * full_command = kmalloc(MAX_ARG_LENGTH, GFP_KERNEL);
-    if (unlikely(!full_command))
-    {
-        pr_info("Failed to allocate memory for full_command\n");
-        goto free_file_name_and_call_original_execve;
-    }
+    // pr_info("gid: %d uid: %d, argc: %d, binary: '%s', Full command: '%s'\n", gid, uid, argc, filename, full_command);
+    // if(string_compare_with_wildcards("ping*", full_command))
+    // {
+    //     pr_info("Blocked ping\n");
+    //     goto execve_prevention;
+    // }
 
-    int argc = join_strings_from_user(argv, " ", full_command, MAX_ARG_LENGTH);
-    full_command[MAX_ARG_LENGTH - 1] = '\0';
-    pr_info("gid: %d uid: %d, argc: %d, binary: '%s', Full command: '%s'\n", gid, uid, argc, filename, full_command);
-    if(string_compare_with_wildcards("ping*", full_command))
-    {
-        pr_info("Blocked ping\n");
-        goto execve_prevention;
-    }
-
-    kfree(full_command);
-free_file_name_and_call_original_execve:
-    kfree(filename);
 call_original_execve:
     return original_execve(regs);
 
 execve_prevention:
-        kfree(full_command);
-        kfree(filename);
         return -EPERM;
 }
 
