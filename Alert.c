5b21b229d80b4742e5145db86e7ce53520dfdcc0
SilverPlate3 <arielsilver77@gmail.com>
2024-03-25 08:16:12 +0200

    Remove Netlink related stuff from Alert.*

--- Alert.c
+++ Alert.c
@@ -1,11 +1,6 @@
 #include "Alert.h"
-
-#include <linux/skbuff.h>
-#include <net/sock.h>
-#include <linux/netlink.h>
-
-DEFINE_MUTEX(netlink_pipe_mutex);
-struct sock *nl_sk = NULL;
+#include "Netlink.h"
+#include "NetlinkSettings.h"
 
 static struct alert* create_alert_execve(struct rule *rule, execve_event * execve)
 {
@@ -20,14 +15,6 @@ static struct alert* create_alert_execve(struct rule *rule, execve_event * execv
     alert->event.execve = *execve;
 
     return alert;
-}
-
-static int thread_safe_nlmsg_unicast(struct sock *sk, struct sk_buff *skb, u32 portid)
-{
-    mutex_lock(&netlink_pipe_mutex);
-    int rv = nlmsg_unicast(sk, skb, portid);
-    mutex_unlock(&netlink_pipe_mutex);
-    return rv;
 }
 
 static void send_alert(struct alert *alert)
@@ -57,26 +44,5 @@ void execve_alert(struct rule *rule, execve_event * execve)
     {
         send_alert(alert);
         kfree(alert);
-    }
-}
-
-int netlink_register(void)
-{
-    struct netlink_kernel_cfg cfg = {.input = NULL,};
-    nl_sk = netlink_kernel_create(&init_net, NETLINK_GOOD_KIT, &cfg);
-    if(!nl_sk)
-    {
-        pr_info("netlink_kernel_create failed to create netlink socket\n");
-        return 0;
-    }
-
-    return 1;
-}
-
-void netlink_unregister(void)
-{
-    if(nl_sk)
-    {
-        netlink_kernel_release(nl_sk);
     }
 }
--- Alert.h
+++ Alert.h
@@ -2,9 +2,6 @@
 
 #include "Rules.h"
 
-#define NETLINK_GOOD_KIT 31
-#define NETLINK_PORT_ID 1111
-
 struct alert
 {
     struct rule rule;
@@ -15,11 +12,5 @@ struct alert
 };
 
 #ifdef __KERNEL__
-
 void execve_alert(struct rule *rule, execve_event * execve);
-
-int netlink_register(void);
-
-void netlink_unregister(void);
-
 #endif
