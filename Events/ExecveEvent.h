ed8de5ead31525744861f6306b181e9acf04c9f0
SilverPlate3 <arielsilver77@gmail.com>
2024-03-29 12:42:50 +0300

    added TODO

--- Events/ExecveEvent.h
+++ Events/ExecveEvent.h
@@ -5,6 +5,6 @@
 #include <linux/kernel.h> // for struct pt_regs
 
 #define MKVAR(Type, Name, From) Type Name = (Type)(From);
-#define MAX_ARG_LENGTH 1024
+#define MAX_ARG_LENGTH 1024 // TODO: is this really needed? as execve_rule.full_command is PATH_MAX
 
 execve_event * create_execve_event(const struct pt_regs *regs);
