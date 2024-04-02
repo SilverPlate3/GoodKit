dee361c8cbe06533fea1b49f273a5f9ff4cf3678
SilverPlate3 <arielsilver77@gmail.com>
2024-04-02 09:35:23 +0300

    add TODO

--- Rules/RulesIoctl.h
+++ Rules/RulesIoctl.h
@@ -2,7 +2,7 @@
 
 #include <linux/fs.h>
 
-long good_kit_rules_ioctl_main_callback(struct file *file, unsigned int ioctl_num, unsigned long parameter);
+long good_kit_rules_ioctl_main_callback(struct file *file, unsigned int ioctl_num, unsigned long parameter); // TODO: Check if decleration is really needed.
 
 int register_rules_device(void);
 
